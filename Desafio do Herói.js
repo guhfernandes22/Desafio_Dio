9581267fedae1a20941767db629e723af683e457
guhfernandes22 <gustavo.b.fernandes@outlook.com>
2023-09-14 22:48:52 -0300

    desafio finalizado

---
+++ Desafio do Herói.js
@@ -0,0 +1,19 @@
+let heroi = "Gustavinho";
+let experiencia = 10001;
+
+if (experiencia <= 1000) {
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Ferro");
+}else if(experiencia >=1001 && experiencia <=2000){
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Bronze");
+}else if(experiencia >=2001 && experiencia <=5000){
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Prata");
+}else if(experiencia >=6001 && experiencia <=7000){
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Ouro");
+}else if(experiencia >=7001 && experiencia <=8000){
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Platina");
+}else if(experiencia >=8001 && experiencia <=9000){
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Ascente");
+}else if(experiencia >=9001 && experiencia <=10000){
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Imortal");
+}else if(experiencia >=10001)
+    console.log("O Herói vulgo o nome " + heroi + " está no nível Radiante")
